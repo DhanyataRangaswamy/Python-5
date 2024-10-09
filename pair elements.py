e1686bd1924c5de03a2c2ab63d3d8aea523bad58
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-10-09 18:20:52 +0530

    pair elements

---
+++ pair elements.py
@@ -0,0 +1,10 @@
+class pairelements:
+    def twosum(self,nums,target):
+        lookup={}
+
+        for i , num in enumerate(nums):
+            if target-num in lookup:
+             return(lookup[target-num],i)
+            lookup[num]=i
+value=int(input("Enter the value of sum for which u want to make this search :"))
+print("index1=%d ,index2=%d" % pairelements().twosum((10,20,30,40,50,60,70),value))
